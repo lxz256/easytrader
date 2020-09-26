a02f96228335945c3e2cfd2d57c16d78ad7dcd18
liuxz-pc <lxz256@163.com>
2020-09-26 22:51:17 +0800

    bugfix

--- easytrader/pop_dialog_handler.py
+++ easytrader/pop_dialog_handler.py
@@ -51,15 +51,10 @@ class PopDialogHandler:
         # 复选框
         if self._app.top_window().child_window(control_id=1504, class_name='Button').exists():
             self._app.top_window().child_window(control_id=1504, class_name='Button').click()
-        # 点击确定或确认
         if self._app.top_window()["确定"].exists():
             self._app.top_window()["确定"].click()
         else:
             self._app.top_window()["确认"].click()
-        # except Exception as ex:
-        #     self._app.window(
-        #         best_match="Dialog", top_level_only=True
-        #     ).child_window(best_match="确定").click()
 
     def _submit_by_shortcut(self):
         self._set_foreground(self._app.top_window())
@@ -117,30 +112,16 @@ class TradePopDialogHandler(PopDialogHandler):
         # 银河基金信息披露和风险确认
         if title == "基金信息披露":
             self._app.top_window()['基金信息披露Shell DocObject View'].click()
-            time.sleep(0.2)
+            time.sleep(0.1)
             self._app.top_window().type_keys('{TAB}')
-            time.sleep(0.2)
+            time.sleep(0.1)
+            self._app.top_window().type_keys('{TAB}')
+            time.sleep(0.1)
             self._app.top_window().type_keys("{ENTER}")
-
-            retry = 20
-            while retry:
-                try:
-                    self._app.top_window().child_window(control_id=4427, class_name='Button').wait("ready", timeout=0.5,
-                                                                                                   retry_interval=0.2)  # 保存
-                    break
-                except RuntimeError:
-                    retry -= 1
-                    logger.info('con not find save button, retry%s ' % (20 - retry))
-
-            self._app.top_window().type_keys("{ESC}")
-            time.sleep(0.5)
+            time.sleep(0.1)
             self._submit_by_click()
             return None
 
-        # 银河风险确认弹窗
-        if title == "风险确认":
-            self._submit_by_click()
-            return None
         # 银河风险告知
         if title == "公募证券投资基金投资风险告知":
             self._submit_by_click()
