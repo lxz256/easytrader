262cf5f3e09ad7c032d92e05892520903d43bb95
liuxz-pc <lxz256@163.com>
2020-09-27 07:46:40 +0800

    debug

--- tests/hton.py
+++ tests/hton.py
@@ -12,18 +12,21 @@ import easytrader
 
 user = easytrader.use('yh_client')
 user.prepare(user='213600010660', password='126259')
-time.sleep(3)
+time.sleep(1)
+print(user._app.top_window().child_window(control_id=1504, class_name='Button').window_text())
 
+# user.purchase(security='162411', amount=100)
 
-# todo 工作日检查
-# todo 溢价检查
-# todo 逐账户申购
-selects = user.main.child_window(
-    control_id=user.config.ACCOUNT_CONTROL_ID, class_name="ComboBox"
-)
-for i in range(len(set(selects.texts()))):
-    selects.select(i)
-    o = user.position
-    print(o)
+# # todo 工作日检查
+# # todo 溢价检查
+# # todo 逐账户申购
+# selects = user.main.child_window(
+#     control_id=user.config.ACCOUNT_CONTROL_ID, class_name="ComboBox"
+# )
+# for i in range(len(set(selects.texts()))):
+#     selects.select(i)
+# o = user.position
+# print(o)
 
-
+# print(user.app.window(class_name="#32770", visible_only=True))
+# print(user.app.top_window().child_window(control_id=1365).window_text())
